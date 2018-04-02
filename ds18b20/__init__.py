d3def2b0b926f790b28060f35f587916a87b3c88
Wytamma Wirth <wytamma.wirth@me.com>
2018-04-02 12:59:47 +1000

    some fixes and imports

--- ds18b20/__init__.py
+++ ds18b20/__init__.py
@@ -1,10 +1,13 @@
+import os
+import fnmatch
+import time
 
 def get_all_probes(self):
     """return list of probes"""
-    return [Probe(probe_id) for probe_id in os.listdir("/sys/bus/w1/devices") if fnmatch.fnmatch(filename, '28-*')]    
+    return [Probe(probe_id) for probe_id in os.listdir("/sys/bus/w1/devices") if fnmatch.fnmatch(probe_id, '28-*')]    
 
 def get_probe_by_id(probe_id):
-    if os.path.exists('/sys/bus/w1/devices/{}/w1_slave'.format(self.probe_id)):
+    if os.path.exists('/sys/bus/w1/devices/{}/w1_slave'.format(probe_id)):
         return Probe(probe_id)
 
 class Probe:
@@ -19,15 +22,15 @@ class Probe:
 
     def mean_temperature(self, n):
         temps = []
-        for i in range(n):
-            temp = _get_temp()
+        for _ in range(n):
+            temp = self._get_temp()
             if temp == None:
                 return
             temps.append(temp)
         return sum(temps)/n
     
     def _get_temp(self):  # TODO: async?
-        for i in range(3):
+        for _ in range(3):
             with open(self._probe_addr, 'r') as f_obj:
                 lines = f_obj.readlines()
                 if lines[0].find("YES") is -1:
