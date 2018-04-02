838be78dc6dcb1ee395474ea97030e1c40984660
Wytamma Wirth <wytamma.wirth@me.com>
2018-04-02 14:20:14 +1000

    factor

--- tests/example_script.py
+++ tests/example_script.py
@@ -1,16 +1,19 @@
 from .context import ds18b20
 import time
 
-print('ds18b20.all_probes:', ds18b20.all_probes())
-p = ds18b20.all_probes()[0]
+def script():
+    print('ds18b20.all_probes:', ds18b20.all_probes())
+    p = ds18b20.all_probes()[0]
 
-print('ds18b20.probe_by_id:', ds18b20.probe_by_id(p.probe_id))
+    print('ds18b20.probe_by_id:', ds18b20.probe_by_id(p.probe_id))
 
-temp = p.read_temperature()
-print('Probe.read_temperature:', temp)
+    temp = p.read_temperature()
+    print('Probe.read_temperature:', temp)
 
-print('Wating for change...')
-while not p.changed():
-   time.sleep(1)
-print('Chnaged!', temp - p.read_temperature())
+    print('Wating for change...')
+    while not p.changed():
+    time.sleep(1)
+    print('Chnaged!', temp - p.read_temperature())
 
+if __name__ == '__main__':
+    script()
